cea5905df1e60e88ada89d20ea2ba6dc59e7edc0
Robin-Oak <69987305+Robin-Oak@users.noreply.github.com>
2020-08-30 00:51:28 +0530

    Update assg2_6.java

--- assg2/assg2_6.java
+++ assg2/assg2_6.java
@@ -4,12 +4,37 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
-        long n = obj.nextInt();
-        while (n != 1) {
-            if (n % 2 == 0)
-                n /= 2;
-            else n = n * 3 + 1;
-            System.out.println(n);
+        String n = obj.nextLine();
+        int flag=0;
+        do {
+            flag=0;
+            for (int i = 0; i < n.length(); i++)
+                if (n.charAt(i) <= '0' || n.charAt(i) >= '9') {
+                    flag = 1;
+                    break;
+                }
+            if (flag == 1) {
+                System.out.println("Better chose a positive number. Try again?");
+                n = obj.nextLine();
+            }
+        }
+        while(flag!=0);
+        long nk=Long.valueOf(n);
+        while(nk<0) {
+            System.out.println("Negative numbers are so not allowed. Better chose a positive number. Try again?");
+            nk = obj.nextInt();
+        }
+        while (nk != 1) {
+            if (nk % 2 == 0)
+                nk /= 2;
+            else {
+                nk = nk * 3 + 1;
+            }
+            System.out.println(nk);
         }
     }
 }
+//Integer n=new Integer(string);
+//valueOf
+//int a=Integer.valueOf(string);
+//string buffer class
