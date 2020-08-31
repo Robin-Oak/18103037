0befbba594766094c2a7c2a2aca132b0bf99b788
Robin-Oak <69987305+Robin-Oak@users.noreply.github.com>
2020-08-31 18:10:37 +0530

    Update assg2_4.java

--- assg2/assg2_4.java
+++ assg2/assg2_4.java
@@ -1,10 +1,10 @@
 public class Main {
-
     public static void main(String[] args) {
-        long sum=0, i=1;
+        int sum=0, i=1;
         for(; i<Integer.MAX_VALUE; i++) {
+            if(sum >= Integer.MAX_VALUE-i) break;
             sum+=i;
-            if(sum==i*i)
+            if((int)Math.sqrt(sum)==i)
                 System.out.println(i);
         }
         System.out.println("The program ended at "+i);
