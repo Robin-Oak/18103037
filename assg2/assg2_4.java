2edf1aa865995bf78a6bf57a861176843015d4fe
Robin-Oak <69987305+Robin-Oak@users.noreply.github.com>
2020-08-31 11:59:03 +0530

    Update assg2_4.java

--- assg2/assg2_4.java
+++ assg2/assg2_4.java
@@ -1,16 +1,12 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        int sum=0, i=1;
-        for(; sum<Integer.MAX_VALUE && i<Integer.MAX_VALUE; i++) {
-            if(sum+i>=Integer.MAX_VALUE)
-                break;
+        long sum=0, i=1;
+        for(; i<Integer.MAX_VALUE; i++) {
             sum+=i;
             if(sum==i*i)
                 System.out.println(i);
         }
-        //System.out.println("The program ended at "+i);
+        System.out.println("The program ended at "+i);
     }
 }
